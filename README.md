606887d771160f2eedd38aed3fa377e97f2b6adf
mateusz.kaczmarek <mateusz.kaczmarek@globegroup.pl>
2019-03-20 13:27:24 +0100

    Mercure initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Symfony test project
+# Symfony test project
+
+## Testing
+
+1. Open http://symfonytests.lh/test/publish
+2. Now open http://symfonytests.lh/test/receive and 
+## Generate new JWT Key:
+
+You must change **your-256-bit-secret** to your secret provided in **_develop/.env**
+
+[https://jwt.io/](https://jwt.io/#debugger-io?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJtZXJjdXJlIjp7InB1Ymxpc2giOltdfX0.473isprbLWLjXmAaVZj6FIVkCdjn37SQpGjzWws-xa0)
+
---
+++ symfony/src/Controller/PublishController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mercure\Publisher;
+use Symfony\Component\Mercure\Update;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class PublishController
+ * @package App\Controller
+ */
+class PublishController
+{
+    /**
+     * @Route(
+     *     name="test_publish",
+     *     path="test/publish"
+     * )
+     *
+     * @param Publisher $publisher
+     *
+     * @throws
+     *
+     * @return Response
+     */
+    public function testPush(Publisher $publisher): Response
+    {
+        $date = new DateTime('now');
+        $update = new Update(
+            'test/1',
+            json_encode([
+                'message' => 'Hello World! ' . $date->format('c')
+            ])
+        );
+
+        // The Publisher service is an invokable object
+        $publisher($update);
+
+        return new Response('Message ' . $update->getData() . ' published!');
+    }
+
+    /**
+     * @Route(
+     *     name="test_receive",
+     *     path="test/receive"
+     * )
+     *
+     * @return Response
+     */
+    public function testReceive()
+    {
+        return new Response(
+            '<!doctype html><html lang="en"><head><meta charset="utf-8"><title>Mercury subscriber</title></head>
+
+            <body><h1>Mercury subscriber</h1><div id="output" style="border:1px solid black; overflow:scroll; height: 250px;"></div>
+            
+            <script type="text/javascript">
+            const es = new EventSource(\'http://symfonytests.lh:3000/hub?topic=\' + encodeURIComponent(\'test/1\'));
+            es.onmessage = e => {
+                // Will be called every time an update is published by the server
+                let div = document.getElementById(\'output\');
+                div.innerHTML += \'<p>\' + JSON.parse(e.data).message + \'</p>\';
+            }
+            </script>
+            
+            </body></html>'
+        );
+    }
+}
